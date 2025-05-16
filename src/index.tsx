edff5e905b161f48743e5f1b5c92cab61801d2c6
Aike Terjung <at@akt-web.de>
2025-05-17 00:19:27 +0200

    moved styles to scss module

--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,24 @@
-import React from 'react';
+import React from "react";
+import ReactSelect from "react-select";
+import { StateManagerProps } from "react-select/dist/declarations/src/useStateManager";
 
-import ReactSelect, { mergeStyles } from "react-select";
-import { StateManagerProps } from 'react-select/dist/declarations/src/useStateManager';
+export type ReactSelectBootstrapProps = StateManagerProps & {
+  isInvalid?: boolean;
+};
 
-export type ReactSelectBootstrapProps = StateManagerProps
-  & {
-    isInvalid?: boolean
-  }
-
-export const ReactSelectBootstrap = ({ isInvalid = false, ...props }: ReactSelectBootstrapProps) => {
-
-  return <ReactSelect {...props}
-    styles={
-      mergeStyles(
-        {
-          control: (styles: any) => ({
-            ...styles,
-            borderRadius: "var(--bs-border-radius)",
-            ...(
-              !isInvalid
-                ? {
-                  borderColor: "#ced4da",
-                  '&:hover': {
-                    border: "1px solid #ced4da"
-                  },
-                  '&:focus-within': {
-                    boxShadow: "0 0 0 0.25rem rgba(13, 110, 253, 0.25)",
-                    borderColor: "#86b7fe"
-                  }
-                }
-                : {
-                  borderColor: "var(--bs-red)",
-                  '&:hover': {
-                    border: "1px solid var(--bs-red)"
-                  },
-                  '&:focus-within': {
-                    boxShadow: "0 0 0 0.25rem rgba(var(--bs-danger-rgb), 0.25)",
-                    borderColor: "var(--bs-red)"
-                  }
-                }
-            )
-          }),
-          indicatorsContainer: (styles: any) => ({
-            ...styles,
-            '& > div': {
-              color: "#343a40",
-            },
-            '& > div:hover': {
-              color: "#343a40"
-            }
-          })
-        },
-        props.styles
-      )
-    }
+export const ReactSelectBootstrap = ({
+                                       isInvalid = false,
+                                        classNames = {},
+                                       ...props
+                                     }: ReactSelectBootstrapProps) => (
+  <ReactSelect
+    {...props}
+    unstyled
+    classNamePrefix="react-select"
+    className={`react-select-control ${isInvalid ? "is-invalid" : ""}`}
+    classNames={{  // Merge the provided classNames with the default
+      indicatorsContainer: classNames.indicatorsContainer || (() => "react-select-indicators"),
+      ...classNames, // Spread to include all other potential overrides
+    }}
   />
-}
+);
