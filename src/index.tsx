48e136e1f47b3f0a448321937fe61af991d2bcb9
Aike Terjung <at@akt-web.de>
2025-05-26 15:21:43 +0200

    Implement async switch

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactSelect from "react-select";
+import AsyncSelect from 'react-select/async';
 import { StateManagerProps } from "react-select/dist/declarations/src/useStateManager";
 
 export type ReactSelectBootstrapProps = StateManagerProps & {
@@ -8,17 +9,22 @@ export type ReactSelectBootstrapProps = StateManagerProps & {
 
 export const ReactSelectBootstrap = ({
                                        isInvalid = false,
+                                        async = false,
                                         classNames = {},
                                        ...props
-                                     }: ReactSelectBootstrapProps) => (
-  <ReactSelect
-    {...props}
-    unstyled
-    classNamePrefix="react-select"
-    className={`react-select-control ${isInvalid ? "is-invalid" : ""}`}
-    classNames={{  // Merge the provided classNames with the default
-      indicatorsContainer: classNames.indicatorsContainer || (() => "react-select-indicators"),
-      ...classNames, // Spread to include all other potential overrides
-    }}
-  />
-);
+                                     }: ReactSelectBootstrapProps) => {
+
+  const Component = async ? AsyncSelect : ReactSelect;
+  return (
+    <Component
+      {...props}
+      unstyled
+      classNamePrefix="react-select"
+      className={`react-select-control ${isInvalid ? "is-invalid" : ""}`}
+      classNames={{  // Merge the provided classNames with the default
+        indicatorsContainer: classNames.indicatorsContainer || (() => "react-select-indicators"),
+        ...classNames, // Spread to include all other potential overrides
+      }}
+    />
+  );
+}
